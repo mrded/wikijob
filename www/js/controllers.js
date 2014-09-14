18ce1d8ac340da88c96107d0d3d8dadbae7a5e0c
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-09-14 13:54:38 +0100

    Process the button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,20 @@
 angular.module('wj.controllers', [])
 
 .controller('JobsCtrl', function($rootScope, $scope, $ionicLoading, Jobs) {
-  $ionicLoading.show({template: 'Download jobs'});
-  
-  Jobs.reload().then(function() {
-    $ionicLoading.show({template: 'Load jobs'});
+  $scope.reload = function() {
+    $ionicLoading.show({template: 'Download jobs'});
     
-    Jobs.all().then(function(jobs) {
-      $rootScope.jobs = jobs;
-      $ionicLoading.hide();
+    Jobs.reload().then(function() {
+      $ionicLoading.show({template: 'Load jobs'});
+      
+      Jobs.all().then(function(jobs) {
+        $rootScope.jobs = jobs;
+        $ionicLoading.hide();
+      });
     });
-  });
+  };
   
+  $rootScope.jobs = []; 
 })
 
 .controller('JobDetailCtrl', function($scope, $stateParams, Jobs) {
