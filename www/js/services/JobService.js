20eccaf15697f9e56dee308ac16c7263109050ac
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-11-09 20:21:30 +0000

    Refactor to use function instead of variable.

--- www/js/services/JobService.js
+++ www/js/services/JobService.js
@@ -4,7 +4,7 @@ angular.module('wj.services').factory('JobService', function($http, $q, PouchSer
   return {
     all: function(db) {
       var deferred = $q.defer();
-      db = db || PouchService.db;
+      db = db || PouchService.db();
 
       db.query(function(doc) { emit(doc.type); }, {key: 'job', include_docs: true}, function(err, response) {
         if (err) console.log('Cannot load jobs', err);
@@ -19,7 +19,7 @@ angular.module('wj.services').factory('JobService', function($http, $q, PouchSer
 
     load: function(industry, db) {
       var deferred = $q.defer();
-      db = db || PouchService.db;
+      db = db || PouchService.db();
 
       var map = function(doc) {
         for (var i = 0; i < doc.job_role.length; ++i) {
@@ -40,7 +40,7 @@ angular.module('wj.services').factory('JobService', function($http, $q, PouchSer
 
     get: function(jobId, db) {
       var deferred = $q.defer();
-      db = db || PouchService.db;
+      db = db || PouchService.db();
 
       db.get(jobId, function(err, response) {
         if (err) console.log('Cannot get the job', err);
@@ -53,7 +53,7 @@ angular.module('wj.services').factory('JobService', function($http, $q, PouchSer
 
     save: function(jobs, db) {
       var deferred = $q.defer();
-      db = db || PouchService.db;
+      db = db || PouchService.db();
 
       db.bulkDocs({docs: jobs}, function(err) {
         if (err) console.log('Cannot save jobs', err);
--- www/js/services/PouchService.js
+++ www/js/services/PouchService.js
@@ -4,16 +4,15 @@ angular.module('wj.services').service('PouchService', function($q) {
   var db_name = 'wikijob';
   var _db = new PouchDB(db_name);
 
-  var WJ = this;
-
-  WJ.db = _db;
+  this.db = function() {
+    return _db;
+  };
 
   this.reset = function() {
     var deferred = $q.defer();
 
     _db.destroy(function() {
       _db = new PouchDB(db_name);
-      WJ.db = _db;
 
       deferred.resolve(_db);
     });
