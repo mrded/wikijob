1f4dc64af41d7bed65eebe15e1c90eb69ffa451a
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-11-09 20:13:42 +0000

    Fix PouchService. Thanks to @qwertmax!

--- www/js/services/PouchService.js
+++ www/js/services/PouchService.js
@@ -4,13 +4,16 @@ angular.module('wj.services').service('PouchService', function($q) {
   var db_name = 'wikijob';
   var _db = new PouchDB(db_name);
 
-  this.db = _db;
+  var WJ = this;
+
+  WJ.db = _db;
 
   this.reset = function() {
     var deferred = $q.defer();
 
     _db.destroy(function() {
       _db = new PouchDB(db_name);
+      WJ.db = _db;
 
       deferred.resolve(_db);
     });
