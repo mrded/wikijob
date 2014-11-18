837e0cd2ff5525158560c450c7788283b2e00658
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-11-18 20:45:31 +0000

    Clean up.

--- www/js/helpers.js
+++ www/js/helpers.js
@@ -1,19 +1,5 @@
 'use strict';
 
-Array.prototype.unique = function() {
-  var a = this.concat();
-
-  for (var i=0; i<a.length; ++i) {
-    for (var j=i+1; j<a.length; ++j) {
-      if (JSON.stringify(a[i]) === JSON.stringify(a[j])) {
-        a.splice(j, 1);
-      }
-    }
-  }
-
-  return a;
-};
-
 function convertImgToBase64(url, callback, outputFormat) {
   var canvas = document.createElement('CANVAS');
   var ctx = canvas.getContext('2d');
