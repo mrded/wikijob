f0f5e2e19814b2ce734ba9f62a9a0829edd8f73e
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-01-08 19:49:23 +0000

    Add load all option.

--- www/js/controllers/MenuCtrl.js
+++ www/js/controllers/MenuCtrl.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('wj.controllers').controller('MenuCtrl', function($rootScope, $scope, $ionicLoading, JobService, IndustryService) {
+
+  $scope.all = function() {
+    $ionicLoading.show({template: 'Loading'});
+
+    JobService.all().then(function(jobs) {
+      $rootScope.jobs = jobs;
+      $ionicLoading.hide();
+    }, function() {
+      $ionicLoading.hide();
+    });
+  };
+
   $scope.load = function(industryId) {
     $ionicLoading.show({template: 'Loading'});
 
